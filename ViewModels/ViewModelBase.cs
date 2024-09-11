477a630124f155c193c891c53c7ec88d1f14b55a
Николай Писклов <koluandino@gmail.com>
2024-09-11 17:28:49 +0300

    Creating ViewModelBase class

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Schedule.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        virtual protected void RaisePropertyChange([CallerMemberName] string? propertyName = null) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public virtual Task LoadAsync() => Task.CompletedTask;  
+    }
+}
