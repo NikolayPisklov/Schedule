4b51545dd9bff7f3c0901f34e024ad13357b7c22
Николай Писклов <koluandino@gmail.com>
2024-09-15 15:18:00 +0300

    Validating user input

--- DataProviders/LoginDataProvider.cs
+++ DataProviders/LoginDataProvider.cs
@@ -5,7 +5,7 @@ namespace Schedule.DataProviders
 {
     public interface ILoginDataProvider 
     {
-        User GetUserByLoginDetailsAsync(string login, string password);
+        User? GetUserByLoginDetailsAsync(string login, string password);
     }
     public class LoginDataProvider : DataProviderBase, ILoginDataProvider
     {
@@ -13,8 +13,6 @@ namespace Schedule.DataProviders
         {
             using(var connection = CreateConnection()) 
             {
-                login = "Admin";
-                password = "admin";
                 var sql = "SELECT * FROM User WHERE Login = @Login AND Password = @Password";
                 var user = connection.Query<User>(sql, 
                     new { Login = login, Password = password }).FirstOrDefault();
--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,20 +1,56 @@
 ﻿using Schedule.Command;
 using Schedule.DataProviders;
+using Schedule.Models;
 using System.Windows;
 
 namespace Schedule.ViewModels
 {
-    public class LoginViewModel : ViewModelBase
+    public class LoginViewModel : ValidationViewModelBase
     {
-        public string? Login { get => _login; set => _login = value; }
-        public string? Password { get => _password; set => _password = value; }
+        public string? Login { 
+            get => _login; 
+            set 
+            { 
+                _login = value;
+                RaisePropertyChange();
+                if (string.IsNullOrEmpty(_login)) 
+                {
+                    AddError("Будь ласка, введіть логін!");
+                }
+                else 
+                {
+                    ClearErrors();
+                }
+            } 
+        }
+        public string? Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                RaisePropertyChange();
+                if (string.IsNullOrEmpty(_password))
+                {
+                    AddError("Будь ласка, введіть пароль!");
+                }
+                else
+                {
+                    ClearErrors();
+                }
+            }
+        }
 
-        private ILoginDataProvider _loginDataProvider;
+        
 
         public DelegateCommand SignInCommand { get; }
 
         private string? _login;
         private string? _password;
+        private readonly ILoginDataProvider _loginDataProvider;
+        private readonly MessageBoxImage _icon = MessageBoxImage.Exclamation;
+        private readonly MessageBoxButton _cancelButton = MessageBoxButton.OK;
+        private readonly string? _loginFailCaption = "Помилка при вході до системи";
         public LoginViewModel(ILoginDataProvider loginDataProvider)
         {
             _loginDataProvider = loginDataProvider;
@@ -22,14 +58,15 @@ namespace Schedule.ViewModels
         }
         private void SignIn(object? parameter) 
         {
-            if(Login is not null && Password is not null) 
+            if (Login is not null && Password is not null)
             {
-                var user = _loginDataProvider.GetUserByLoginDetailsAsync(Login, Password);
-                MessageBox.Show($"{user.Name}, добрий день!");
+                User? user = _loginDataProvider.GetUserByLoginDetailsAsync(Login, Password);
+                if (user is not null) MessageBox.Show($"{user.Name}, добрий день!");
+                else MessageBox.Show("Невірний логін або пароль!", _loginFailCaption, _cancelButton, _icon);
             }
             else 
             {
-                MessageBox.Show("Введіть логін та пароль!");
+                MessageBox.Show("Введіть логін та пароль!", _loginFailCaption, _cancelButton, _icon);
             }
         }
     }
